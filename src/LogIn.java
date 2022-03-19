a8c4038b0e5d1a5924b4a01faf89d3978f9c7a33
Ethan <ethanparkins@gmail.com>
2022-03-19 15:24:18 +0000

    v1

--- src/LogIn.java
+++ src/LogIn.java
@@ -2,6 +2,20 @@
  *
  * @author Ethan
  */
-public class LogIn {
+public class main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("First JavaFX Application");
+        stage.show();
+    }
     
+    public static void main(String[] args){
+        launch(args);
+    }
 }
