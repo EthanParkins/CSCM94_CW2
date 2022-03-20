75315118fe48a4419b75a32f3055d69d1f7e84de
femialogba <femialogba@ymail.com>
2022-03-20 15:57:36 +0000

    v1

---
+++ EatIn.java
@@ -0,0 +1,10 @@
+class EatIn extends Order{
+    private int tableID;
+    private int waiterID;
+
+    public EatIn(){
+        this.tableID = EatIn.getTableID;
+        this.waiterID = EatIn.getwaiterID;
+    }
+    
+}
