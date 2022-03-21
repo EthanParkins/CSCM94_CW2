8875be8096f5c74785e4139ef0c5092209b52368
Ethan <ethanparkins@gmail.com>
2022-03-21 11:42:47 +0000

    v5

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,4 +1,7 @@
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -36,12 +39,31 @@ public class LoginController implements Initializable {
     }
 
     @FXML
-    private void btnLoginClicked(javafx.event.ActionEvent event) {
+    private void btnLoginClicked(javafx.event.ActionEvent event) throws IOException {
+        String userID = tfUserID.getText();
+        String lastName = tfPassword.getText();
+        String filepath = "login.txt"; 
+        /* creates file and adds to it, but only one thing and doesn't read
+        StringBuilder login = new StringBuilder();
+        login.append(tfUserID.getText().toString() + "\n");
+        login.append(tfPassword.getText().toString() + "\n");
+        
+        File loginFile = new File("C:\\Users\\Ethan\\OneDrive\\Comp Sci\\CSCM94\\CW2\\CSCM94_CW2\\login.txt");
+        FileWriter writer = new FileWriter(loginFile);
+        writer.write(login.toString());
+        writer.close();
+        */
+        
+        /*
         //pulls infomation from text fields
         String userID = tfUserID.getText();
         String lastName = tfPassword.getText();
-        
         //test code, will be 'incorrect password' text, when checking ID vs password
-        lbInfo.setText("Welcome " + userID);
+        lbInfo.setText("Welcome " + userID);  
+        */
+    }
+    
+    public static void verifyLogin(String username, String password, String filepath){
+        boolean found = false;
     }
 }
