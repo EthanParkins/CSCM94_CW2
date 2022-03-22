1eaa9d22a6c455330ec9ae975473f4b39e870d56
Ethan <Ethan@137.44.114.73>
2022-03-22 10:48:45 +0000

    v9 - Basic Restaurant Class

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,7 +1,45 @@
+
+import java.util.ArrayList;
+
 /**
+ * A class that creates the restaurant which holds details and stats
  * 
  * @author Ethan
  */
+
 public class Restaurant {
     
+    private static String name = "Cafe94";
+    private ArrayList<Table> tables = new ArrayList<>();
+    private static int capacity = 120;
+    
+    //constructor
+    public Restaurant(){       
+    }
+    
+    //methods
+    
+    /**
+     * Method to return the most popular items ordered
+     * @param orderList 
+     */
+    public void getPopularItems(Order orderList){
+        
+    }
+    
+    /**
+     * Method to return the busiest periods 
+     * @param tableBookingList 
+     */
+    public void getBusyPeroids(Table tableBookingList){
+        
+    }
+    
+    /**
+     * Method to return the most active customers
+     * @param customerHistory 
+     */
+    public void getActiveCustomers(Customer customerHistory){
+        
+    }
 }
